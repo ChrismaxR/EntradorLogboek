3162c6130a3dc48e28dc9edb590924f4e4f84388
Christian van Gaalen <ctmvangaalen@gmail.com>
2025-05-09 13:22:44 +0200

    Evidence project opgezet, tabellen toegevoegd aan datamodel, Readme bijgewerkt

---
+++ code/analyzeData.R
@@ -0,0 +1,19 @@
+source(here::here("code", "wrangleData.R"))
+library(tidytext)
+
+
+# Text analysis
+
+# De twee variabelen met ongestructureerde tekst zijn:
+
+todo$titel
+todo$omschrijving
+
+# ... to do ...
+
+
+
+
+
+  
+
--- code/wrangleData.R
+++ code/wrangleData.R
@@ -10,17 +10,6 @@ logboek_wrangle <- logboek |>
   ) |>
   select(entryId, datum, tijdstempel, everything()) # Zet ID en tijd vooraan
 
-# Selectie van algemene dagkenmerken per logboekentry
-entry <- logboek_wrangle |>
-  select(
-    entryId,
-    datum,
-    tijdstempel,
-    welbevindenVandaag,
-    themasVanVandaag,
-    toolsVanDeDag
-  )
-
 # Extract en harmoniseer todo-items A t/m D in één dataset
 todo <- bind_rows(
   # Todo A
@@ -71,11 +60,81 @@ todo <- bind_rows(
 ) |>
   filter(!is.na(todoTitel)) |> # Verwijder lege todo-items
   transmute(
-    entryId,
     todoId = row_number(), # Unieke ID per todo-item
-    todoVolgorde,
-    todoTitel,
-    todoOmschrijving,
-    todoDevOps,
-    todoType
+    entryId,
+    devOpsId = todoDevOps,
+    titel = todoTitel,
+    type = todoType,
+    volgorde = todoVolgorde,
+    omschrijving = todoOmschrijving
+  )
+
+## themas splitten en long
+themasVanVandaagLong <- entry |> 
+  select(1, 2, themasVanVandaag) |> 
+  mutate(
+    themasVanVandaag = str_split(string = themasVanVandaag, pattern = ", ")
+  ) |> 
+  unnest_longer(themasVanVandaag) |> 
+  View()
+
+## tools splitten en long
+toolsVanDeDagLong <- entry |> 
+  select(1, 2, toolsVanDeDag) |> 
+  mutate(
+    toolsVanDeDag = str_split(string = toolsVanDeDag, pattern = ", ")
+  ) |> 
+  unnest_longer(toolsVanDeDag) |> 
+  View()
+
+
+# Selectie van algemene dagkenmerken per logboekentry
+entry <- logboek_wrangle |> 
+  mutate(
+    welbevindenVandaagBucket = case_when(
+      welbevindenVandaag %in% c(1:6) ~ "laag", 
+      welbevindenVandaag %in% c(7, 8) ~ "normaal", 
+      T ~ "hoog"
+    )
+  ) |> 
+  select(
+    entryId,
+    datum,
+    tijdstempel,
+    welbevindenVandaag,
+    welbevindenVandaagBucket,
+    themasVanVandaag,
+    toolsVanDeDag
+  ) |> 
+  left_join(
+    y = todo |> 
+      group_by(entryId) |> 
+      summarise(
+        todoCount = n()
+      ), by = "entryId" 
+  )
+
+# DevOpsId tracker - doorlooptijden van DevOps PBI's meten
+
+devOpsTracker <- logboek_wrangle |> 
+  select(datum, contains("DevOps")) |> 
+  pivot_longer(cols = 2:5) |> 
+  na.omit() |> 
+  arrange(value, datum) |> 
+  select(-name) |> 
+  rename(devOpsId = value) |> 
+  group_by(devOpsId) |> 
+  mutate(
+    minmax = case_when(
+      datum == max(datum) ~ "maxDatum", 
+      datum == min(datum) ~ "minDatum", 
+      T ~ NA_character_
+    )
+  ) |> 
+  add_tally(name = "aantalDagenGelogd") |> 
+  ungroup() |> 
+  na.omit() |> 
+  pivot_wider(names_from = minmax, values_from = datum) |> 
+  mutate(
+    duratie = maxDatum - minDatum
   )
